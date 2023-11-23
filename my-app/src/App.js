3f69964d2303a0e5f98eb074d647c27016fdb457
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-11-23 08:36:24 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import PricingPage from './components/PricingPage';
 
-function App() {
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <PricingPage />
+      </div>
   );
 }
-
-export default App;
