c2bd0af4f2bee7a910fe5dc871d873c8311b0f7b
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-11-24 11:44:10 +0700

    product

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { FaStar } from 'react-icons/fa';
 // import PricingPage from './components/PricingPage';
-import StarVip from './components/StarRate';
-
+// import StarVip from './components/StarRate';
+import ProductList from './components/ProductList';
+import './css/style.css';
 
 
 export default function MyApp() {
   return (
       <div className="App">
-          <StarVip />
+          <ProductList />
       </div>
   );
 }
