bc3b5dd0bcc1d3e1dfc70b2534735a9ee9781435
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-11-24 10:13:47 +0700

    Star

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { FaStar } from 'react-icons/fa';
+// import PricingPage from './components/PricingPage';
+import StarVip from './components/StarRate';
 
 
 
 export default function MyApp() {
   return (
       <div className="App">
-          < />
+          <StarVip />
       </div>
   );
 }
--- my-app/src/components/StarRate.js
+++ my-app/src/components/StarRate.js
@@ -1,9 +1,8 @@
-import './App.css';
 import { FaStar } from 'react-icons/fa';
 
-const Product = ({ name, price, discount, image, rating, sale }) => {
-    const stars = Array.from({ length: 5 }, (_, i) => (
-        <FaStar key={i} className="star" color={i < rating ? '#ffc107' : '#e4e5e9'} />
+const StarVip = ({ name, price, discount, image, rating, sale }) => {
+    const stars = Array.from({ length: 5 }, (_, index) => (
+        <FaStar key={index} className="star" color={index < rating ? '#ffc107' : '#e4e5e9'} />
     ));
 
     return (
@@ -13,15 +12,19 @@ const Product = ({ name, price, discount, image, rating, sale }) => {
     )
 }
 
-export default function MyApp() {
-    const stars = Array.from({ length: 5 }, (_, index) => (
-        <FaStar key={index} color={index < 3 ? 'Orange' : 'Gray'} />
-    ));
+export default StarVip
 
-    return (
-        <div>
-            <h1>Welcom to my app form Hieu</h1>
-            {stars}
-        </div>
-    );
-}
+// Ham export ben duoi de nhet truc tiep vao App.js ⏬⏬
+
+// export default function MyApp() {
+//     const stars = Array.from({ length: 5 }, (_, index) => (
+//         <FaStar key={index} color={index < 3 ? 'Orange' : 'Gray'} />
+//     ));
+
+//     return (
+//         <div>
+//             <h1>Welcome to my app form Hieu</h1>
+//             {stars}
+//         </div>
+//     );
+// }
