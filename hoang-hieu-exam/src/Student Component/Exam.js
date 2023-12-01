9799ff041a8878f6ce61a7ac2116c19a7988b082
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-12-01 10:05:52 +0700

    /

--- hoang-hieu-exam/src/Student Component/Exam.js
+++ hoang-hieu-exam/src/Student Component/Exam.js
@@ -3,16 +3,12 @@ import React from 'react';
 const Student = ({ id, name, image, email}) => {
     return (
         <div className="card ">
-            <div className="row">
-                <div className="col-4">
                     <img src={image} alt={name} />
                         <div>
                             <h4>{name}</h4>
                             <p>Student id: {id}</p>
                             <h3>{email}</h3>
                     </div>
-                </div>
-            </div>
         </div>
     );
 }
